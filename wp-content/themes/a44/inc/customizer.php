1844714962434e9d8bbf534cb4e8748d724f9c20
andrii44 <andrij-z@yandex.ru>
2016-07-25 15:53:57 -0400

    1-4
    Created WP, connected to database, put _s theme, imported theme unit
    test

---
+++ wp-content/themes/a44/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * a44 Theme Customizer.
+ *
+ * @package a44
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function a44_wpcourse_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'a44_wpcourse_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function a44_wpcourse_customize_preview_js() {
+	wp_enqueue_script( 'a44_wpcourse_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'a44_wpcourse_customize_preview_js' );
---
+++ wp-content/themes/a44/inc/jetpack.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Jetpack Compatibility File.
+ *
+ * @link https://jetpack.com/
+ *
+ * @package a44
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ */
+function a44_wpcourse_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'a44_wpcourse_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+}
+add_action( 'after_setup_theme', 'a44_wpcourse_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function a44_wpcourse_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+		    get_template_part( 'template-parts/content', 'search' );
+		else :
+		    get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+}
